ae21b0f838786c7506eedf2ca2d709ad887113ea
Steven <stnguyen90@gmail.com>
2020-04-05 22:26:54 +0000

    Add views for User, Group, Task, and TaskList

--- tododrf/api/views.py
+++ tododrf/api/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from rest_framework import permissions
 
-# Create your views here.
+from tododrf.api.models import TaskList, Task
+from tododrf.api.serializers import (
+    TaskListSerializer,
+    TaskSerializer,
+    UserSerializer,
+    GroupSerializer
+)
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class TaskListViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows task lists to be viewed or edited.
+    """
+    queryset = TaskList.objects.all()
+    serializer_class = TaskListSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class TaskViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows tasks to be viewed or edited.
+    """
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    permission_classes = [permissions.IsAuthenticated]
