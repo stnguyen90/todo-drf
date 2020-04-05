810edcdd5b4715a2f54f7d623a93c198f5e52ae7
Steven <stnguyen90@gmail.com>
2020-04-05 19:32:35 +0000

    Add TaskList and Task models

--- tododrf/api/models.py
+++ tododrf/api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class TaskList(models.Model):
+    parent = models.ForeignKey('self', on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+
+
+class Task(models.Model):
+    parent = models.ForeignKey('self', on_delete=models.CASCADE)
+    summary = models.CharField(max_length=50)
+    notes = models.CharField(max_length=200)
