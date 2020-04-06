461ea664d4ff295d3fdb785ffd087741b93ba544
Steven <stnguyen90@gmail.com>
2020-04-06 00:31:12 +0000

    Add missing Task List and Task fields

--- tododrf/api/models.py
+++ tododrf/api/models.py
@@ -1,12 +1,15 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
 class TaskList(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
     parent = models.ForeignKey('self', on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
 
 
 class Task(models.Model):
+    task_list = models.ForeignKey(TaskList, on_delete=models.CASCADE)
     parent = models.ForeignKey('self', on_delete=models.CASCADE)
     summary = models.CharField(max_length=50)
     notes = models.CharField(max_length=200)
